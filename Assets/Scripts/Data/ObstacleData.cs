9b695c296211cac09be9fe0f9308010c239060cd
reru96 <renatorumieri96@outlook.com>
2025-09-11 19:38:16 +0200

    ObstacleData interfaccia

--- Assets/Scripts/Data/ObstacleData.cs
+++ Assets/Scripts/Data/ObstacleData.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "ObstacleData", menuName = "ScriptableObjects/ObstacleData")]
-
 public class ObstacleData : ScriptableObject
 {
     public GameObject prefab;
     public int weight;
+
+    public int maxDifficult;
+    public int minDifficult;
+
+    public GameObject Prefab => prefab;
+    public int Weight => weight;
 }

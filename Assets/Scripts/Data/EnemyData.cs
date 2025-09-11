1069e548e3c5abad1c401d873709f514e2b0ab5e
reru96 <renatorumieri96@outlook.com>
2025-09-11 19:36:19 +0200

    EnemyData interfaccia

--- Assets/Scripts/Data/EnemyData.cs
+++ Assets/Scripts/Data/EnemyData.cs
@@ -2,10 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "EnemyData", menuName = "ScriptableObjects/EnemyData")]
 
-public class EnemyData : ScriptableObject
+[CreateAssetMenu(fileName = "EnemyData", menuName = "ScriptableObjects/EnemyData")]
+public class EnemyData : ScriptableObject, IWeighted
 {
     public GameObject prefab;
     public int weight;
+    public int minDifficultyLevel;
+    public int maxDifficultyLevel;
+
+    public GameObject Prefab => prefab;
+    public int Weight => weight;
 }

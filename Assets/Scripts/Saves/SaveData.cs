b9834f599164abefa75665ce78cc30c5dec0e093
EdoCirone <edoardocirone@gmail.com>
2025-09-16 23:07:27 +0200

    modifica curved e tentativo di approccio

--- Assets/Scripts/Saves/SaveData.cs
+++ Assets/Scripts/Saves/SaveData.cs
@@ -3,7 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class SaveData 
+public class SaveData
 {
-   public List<int>highScores = new List<int>();
+    public List<int> highScores = new List<int>();
+    public int sceneIndex;
+    public float playerX, playerY, playerZ;
+    public int score;
+    public int coins;
+    public List<bool> collectables = new List<bool>();
+    public List<BoostSaveData> boosts = new List<BoostSaveData>();
+}
+
+[System.Serializable]
+public class BoostSaveData
+{
+    public string boostName;
+    public int currentLevel;
 }
